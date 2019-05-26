7a0f2093370473ce278b6f994e81fc02791035cd
sanyam-kaul <kaulsanyam@gmail.com>
2019-05-26 16:25:55 +0530

    Made '/fileUpload' link workable

--- controllers/index.js
+++ controllers/index.js
@@ -3,3 +3,7 @@
 exports.index = function(req, res, next) {
   res.render('index', { title: 'Phoenix Data' });
 }
+
+exports.fileUpload = function(req, res, next) {
+  res.render('fileUpload', { title: 'Phoenix Data' });
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 let index = require('../controllers/index');
 
+
 /* GET home page. */
 router.get('/', index.index);
+
+/* GET file upload page. */
+router.get('/fileUpload', index.fileUpload);
 
 module.exports = router;

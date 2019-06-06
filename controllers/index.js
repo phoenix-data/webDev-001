74ec9cb1b50456dc48823eebdc12035da7001d29
sanyam-kaul <kaulsanyam@gmail.com>
2019-06-06 18:37:20 +0530

    Added post route handler

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,25 @@ exports.index = function(req, res, next) {
   res.render('index', { title: 'Phoenix Data' });
 }
 
-exports.fileUpload = function(req, res, next) {
+exports.fileUploadPage = function(req, res, next) {
   res.render('fileUpload', { title: 'Phoenix Data' });
+}
+
+exports.fileUpload = function(req,res,next) {
+    // Document to be inserted
+    const userInput = req.body;
+
+   
+    Joi.validate(userInput,(err,result)=>{
+        if(err)
+        	console.log(err);
+        else{
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err)
+                	console.log(err);
+                else
+                    res.json({result : result, document : result.ops[0],msg : "Successfully inserted file!!!",error : null});
+            });
+        }
+    })    
 }

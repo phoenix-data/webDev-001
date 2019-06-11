effde2fc83a58f1a880678a5c7a9dbb4830924ec
sanyam-kaul <kaulsanyam@gmail.com>
2019-06-11 23:05:56 +0530

    Created storage engine using Multer

--- app.js
+++ app.js
@@ -34,7 +34,26 @@ conn.once('open', () => {
   gfs.collection('files');
 });
 
-
+//Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'files'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
 
 app.get('/', (req, res)=>{
 	res.render('index');

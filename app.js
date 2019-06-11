e511fcfb55ef3b765cfb30b8d68bc36f411c9e7e
sanyam-kaul <kaulsanyam@gmail.com>
2019-06-11 23:03:53 +0530

    Added essential packags and middlewares

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+
 
 const app = express();
 
+
+//Middleware
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
+
+
+
 
 app.get('/', (req, res)=>{
 	res.render('index');
